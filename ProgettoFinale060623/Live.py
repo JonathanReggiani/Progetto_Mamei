f8b9dc1eebaa7e4c7b5a177a8ad82c72ef03f54c
Jonathan Reggiani <reggianijonathan@gmail.com>
2023-06-09 23:25:03 +0200

    16 commit -09/06/2023 clean

--- ProgettoFinale060623/Live.py
+++ ProgettoFinale060623/Live.py
@@ -2,14 +2,14 @@ import time
 from PyP100 import PyP110
 import paho.mqtt.client as mqtt
 
-p110 = PyP110.P110("192.168.0.179", "mindolifrancesco@gmail.com", "ProvaPw99")
+p110 = PyP110.P110("192.168.1.158", "reggianijonathan@gmail.com", "tapoteam1")
 p110.handshake() #Creates the cookies required for further methods
 p110.login() #Sends credentials to the plug and creates AES Key and IV for further methods
 
 defaultTopicE = "/sensor/energia"
 broker_ip = "broker.emqx.io"
 portaMosquito = 1883 #porta specifica nel localhost da cui si lancia il codice
-sensor = 'TV_FM'
+sensor = 'TV_JZ'
 
 #FUNZIONI
 # funzione di callback per la connessione al broker MQTT
@@ -29,8 +29,9 @@ while(run):
     count += 1
     #The P110 has all the same basic functions as the plugs and additionally allow for energy monitoring.
     r = p110.getEnergyUsage() #Returns dict with all of the energy usage of the connected plug
+    print(r)
     date = r['result']['local_time']
-    today_energy = r['result']['today_energy']
+    today_energy = r['result']['today_energy']/1000 #da W a KW
     energy_list.append(today_energy)
     hour = date.split(" ")[1][:2]
     if(hour=='00'): #se lo facciamo al minuto, deve essere 00 anche il minuto
@@ -38,7 +39,9 @@ while(run):
     if(count>1):
         last_energy = energy_list[count-1] - energy_list[count-2]
         date = date.replace('-','/')
+        print(date)
         toFirestore = str(date) + ',' + str(last_energy)
+        print(toFirestore)
         #dati firestore
         print(sensor, 'invio...')
         infot = mqtt_client.publish(f'ProgettoMameiIoT/energia/sensor/{sensor}', f'val={toFirestore}')
